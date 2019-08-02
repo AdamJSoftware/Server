852c154573b911fc72ad2547f92fff7040ec71cc
Adam Jasniewicz <windsoradamj@gmail.com>
2019-08-02 14:10:49 -0400

    Switching to linux

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import os
 def main():
     while True:
         os.system('cls')
-        subprocess.call(['python.exe', 'Multi_Server.py'])
+        subprocess.call(['python', 'Multi_Server.py'])
 
 
 if __name__ == '__main__':
