3c9f42f63258911c09aa86f939569565ce3ff6a2
Adam Jasniewicz <WindsorAdamJ@gmail.com>
2019-12-10 14:40:51 -0800

    fixed server file

--- Multi_Server.py
+++ Multi_Server.py
@@ -7,6 +7,7 @@ import selectors
 import types
 import datetime
 import threading
+import sys
 import gc
 
 from Scripts import File_Sender
@@ -55,9 +56,9 @@ class pc_list():
     def add_port(self):
         for index, val in enumerate(pc_list.instances):
             if val == '--REPLACE--':
-                self.port = (index+1)*3
+                self.client_port = (index+1)*3
                 return
-        self.port = (int(len(pc_list.instances))+1)*3
+        self.client_port = (int(len(pc_list.instances))+1)*3
 
     def add_number(self):
         for index, val in enumerate(pc_list.instances):
@@ -210,77 +211,80 @@ def remove_client_from_list(client_number):
 
 
 def service_connection(key, mask):
-    global accepted
-    temporary_sock = key.fileobj
-    if temporary_sock not in vars(pc_list).items():
-        if accepted is True:
-            random = str(uuid.uuid4())
-            random = random[:4]
-            random = 'temporary' + str(random)
-            # The three lines above are creating a temporary name for the client
-            print('TEMPORARY: {}'.format(temporary_sock))
-            new_client = pc_list('JUST JOINED', str(random), '', get_ip_from_sock(
-                temporary_sock), temporary_sock, '', '', '', '')
-            # pc_list["clientName"] = append_to_pc_list(pc_list["clientName"], str(random))
-            # pc_list["clientSocket"] = append_to_pc_list(pc_list["clientSocket"], sock)
-            # pc_list["clientIP"] = append_to_pc_list(pc_list["clientIP"], get_ip_from_sock(temporary_sock))
-            new_client.add_number()
-            new_client.add_port()
-            # Most of the client details are appended to the pc_list
-            print("Please wait for system to configure new computer...")
-            # FIX THIS WAITING THING FOR MORE SECURE CONNECITION
-            time.sleep(1)
-            client_number = new_client.client_number
+    try:
+        global accepted
+        temporary_sock = key.fileobj
+        if temporary_sock not in vars(pc_list).items():
+            if accepted is True:
+                random = str(uuid.uuid4())
+                random = random[:4]
+                random = 'temporary' + str(random)
+                # The three lines above are creating a temporary name for the client
+                print('TEMPORARY: {}'.format(temporary_sock))
+                new_client = pc_list('JUST JOINED', str(random), '', get_ip_from_sock(
+                    temporary_sock), temporary_sock, '', '', '', '')
+                # pc_list["clientName"] = append_to_pc_list(pc_list["clientName"], str(random))
+                # pc_list["clientSocket"] = append_to_pc_list(pc_list["clientSocket"], sock)
+                # pc_list["clientIP"] = append_to_pc_list(pc_list["clientIP"], get_ip_from_sock(temporary_sock))
+                new_client.add_number()
+                new_client.add_port()
+                # Most of the client details are appended to the pc_list
+                print("Please wait for system to configure new computer...")
+                # FIX THIS WAITING THING FOR MORE SECURE CONNECITION
+                time.sleep(1)
+                client_number = new_client.client_number
 
-            print(client_number)
-            # Note for me, this should be removed in order to avoid another client taking the number
-            # Starting the receive thread for the specific client
-            client_hostname = ""
-            client_name = ""
-            client_MAC = ""
-            print('Waiting to receive computer info')
+                print(client_number)
+                # Note for me, this should be removed in order to avoid another client taking the number
+                # Starting the receive thread for the specific client
+                client_hostname = ""
+                client_name = ""
+                client_MAC = ""
+                print('Waiting to receive computer info')
 
-            while client_hostname == "":
-                try:
-                    recv_data = temporary_sock.recv(1024).decode()
-                    if str(recv_data).__contains__("--PCNAME--||"):
-                        _, client_hostname, client_MAC, client_name = str(
-                            recv_data).split("||")
-                except:
-                    pass
-            print(f'''
-                Hostname: {client_hostname}
-                MAC: {client_MAC}
-                Name: {client_name}''')
-            # temporary_sock.sendall(pc_list["clientPort"][clientNumber].encode("utf-8"))
+                while client_hostname == "":
+                    try:
+                        recv_data = temporary_sock.recv(1024).decode()
+                        if str(recv_data).__contains__("--PCNAME--||"):
+                            _, client_hostname, client_MAC, client_name = str(
+                                recv_data).split("||")
+                    except:
+                        pass
+                print(f'''
+                    Hostname: {client_hostname}
+                    MAC: {client_MAC}
+                    Name: {client_name}''')
+                # temporary_sock.sendall(pc_list["clientPort"][clientNumber].encode("utf-8"))
 
-            # Waiting to receive the rest of the details about the client
-            new_client.client_name = client_name
+                # Waiting to receive the rest of the details about the client
+                new_client.client_name = client_name
 
-            print("NEW CLIENT NAME: {}".format(
-                pc_list.client_name[client_number]))
+                print(f"NEW CLIENT NAME: {new_client.client_name}")
 
-            print('Finished switching name')
-            # Replacing the temporary name with the actual name
-            new_client.client_hostname = client_hostname
-            print('Finished adding hostname')
-            new_client.client_mac = client_MAC
-            print('Finished adding MAC')
-            port = new_client.client_port
-            main_port = int(port) - 2
-            new_client.client_receiver = client_name
-            new_client.client_checker = client_name
-            print(f'Client Number: {new_client.client_number}')
-            # pc_list["clientStarter"][clientNumber] = DedicatedStarter(main_port, clientNumber, temporary_sock)
-            # print('Starting dedicated starter')
-            # pc_list["clientStarter"][clientNumber].start()
+                print('Finished switching name')
+                # Replacing the temporary name with the actual name
+                new_client.client_hostname = client_hostname
+                print('Finished adding hostname')
+                new_client.client_mac = client_MAC
+                print('Finished adding MAC')
+                port = new_client.client_port
+                print(port)
+                main_port = int(port) - 2
+                new_client.client_receiver = client_name
+                new_client.client_checker = client_name
+                print(f'Client Number: {new_client.client_number}')
+                # pc_list["clientStarter"][clientNumber] = DedicatedStarter(main_port, clientNumber, temporary_sock)
+                # print('Starting dedicated starter')
+                # pc_list["clientStarter"][clientNumber].start()
 
-            dedicated_starter(new_client, main_port,
-                              client_number, temporary_sock)
+                dedicated_starter(new_client, temporary_sock)
 
-            sel.unregister(temporary_sock)
-            temporary_sock.close()
-            print('CLOSED CONNECTION WITH SOCK')
+                sel.unregister(temporary_sock)
+                temporary_sock.close()
+                print('CLOSED CONNECTION WITH SOCK')
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print("Error: {} at line {}".format(e, exc_tb.tb_lineno))
 
 
 class Receive(Thread):
@@ -427,67 +431,69 @@ class Checker2(Thread):
                 return
 
 
-def dedicated_starter(client_instance, dedicated_port, client_number, temporary_sock):
-    print("SYSTEM: Starting dedicated server")
-    print("USING PORT: {}".format(dedicated_port))
-    host = ''
-    port = int(dedicated_port)
-
-    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
+def dedicated_starter(client_instance, temporary_sock):
     try:
-        server_sock.bind((host, port))
-    except Exception as error:
-        print("Bind failed. Error : " + str(error))
-        time.sleep(1)
-        # server_restart()
+        print("SYSTEM: Starting dedicated server")
+        print(f"USING PORT: {client_instance.client_port}")
+        host = ''
 
-    server_sock.listen(5)
-    print("SYSTEM: Socket created")
+        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    temporary_sock.sendall(
-        str(f"--PORT--{dedicated_port.encode('utf-8')}"))
-    print('Finished sending info')
-    # server_sock.setblocking(False)
+        try:
+            server_sock.bind((host, client_instance.client_port))
+        except Exception as error:
+            print("Bind failed. Error : " + str(error))
+            time.sleep(1)
+            # server_restart()
 
-    try:
-        # Establish connection with client.
-        connection, address = server_sock.accept()
-        print('SERVER_SOCK: {}'.format(server_sock))
+        server_sock.listen(5)
+        print("SYSTEM: Socket created")
 
-        print('Got connection from', address)
-        connection.setblocking(False)
-        sock = connection
-        client_instance.client_sock = sock
-        print('Finished __init__')
-        # data = types.SimpleNamespace(addr=address, inb=b'', outb=b'')
-        # events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        # self.sock = events[0].fileobj
-        # print('replacing socket')
-        # pc_list["clientSocket"] = replace_socket(pc_list["clientSocket"], self.client_number, self.sock)
-        # print('finished replacing socket')
+        temporary_sock.sendall((f"--PORT--{client_instance.client_port}".encode('utf-8')))
+        print('Finished sending info')
+        # server_sock.setblocking(False)
 
-        print('Initiaiting receiver')
-        client_instance.client_receiver = Receive(
-            int(dedicated_port) + 2, sock)
-        print('starting receiver')
-        client_instance.client_receiver.start()
-        print('Initiaiting checker')
-        checker_port = int(dedicated_port) + 1
-        client_instance.client_checker = Checker(str(checker_port), client_number,
-                                                 pc_list.client_receiver[client_number], client_instance)
-        print('starting checker')
-        client_instance.client_checker.start()
-        print('everything finished succesfully')
-        # checker_port2 = int(self.dedicated_port) + 2
-        # pc_list["clientChecker2"][self.client_number] = Checker2(str(checker_port2))
-        # print('starting checker')
-        # pc_list["clientChecker2"][self.client_number].start()
-        print('everything finished succesfully')
+        try:
+            # Establish connection with client.
+            connection, address = server_sock.accept()
+            print('SERVER_SOCK: {}'.format(server_sock))
 
+            print('Got connection from', address)
+            connection.setblocking(False)
+            sock = connection
+            client_instance.client_sock = sock
+            print('Finished __init__')
+            # data = types.SimpleNamespace(addr=address, inb=b'', outb=b'')
+            # events = selectors.EVENT_READ | selectors.EVENT_WRITE
+            # self.sock = events[0].fileobj
+            # print('replacing socket')
+            # pc_list["clientSocket"] = replace_socket(pc_list["clientSocket"], self.client_number, self.sock)
+            # print('finished replacing socket')
+
+            print('Initiaiting receiver')
+            client_instance.client_receiver = Receive(
+                int(client_instance.client_port) + 2, sock)
+            print('starting receiver')
+            client_instance.client_receiver.start()
+            print('Initiaiting checker')
+            checker_port = int(client_instance.client_port) + 1
+            client_instance.client_checker = Checker(str(checker_port), client_instance.client_number,
+                                                     client_instance.client_receiver, client_instance)
+            print('starting checker')
+            client_instance.client_checker.start()
+            print('everything finished succesfully')
+            # checker_port2 = int(self.dedicated_port) + 2
+            # pc_list["clientChecker2"][self.client_number] = Checker2(str(checker_port2))
+            # print('starting checker')
+            # pc_list["clientChecker2"][self.client_number].start()
+            print('everything finished succesfully')
+
+        except Exception as e:
+            print(e)
     except Exception as e:
-        print(e)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print("Error: {} at line {}".format(e, exc_tb.tb_lineno))
 
 
 def send_message():
@@ -563,7 +569,7 @@ def main():
         try:
             user_input = input(" -> ")
             if user_input == "/ls -all":
-                for instance in pc_list.instancess:
+                for instance in pc_list.instances:
                     print(f'NAME: {instance.client_name}')
                     for (key, value) in instance.items():
                         print(f'{key} => {value}')
